9b2c32731b30b905fd008453e576977d78f53909
nijoj <nijo.email@gmail.com>
2018-01-15 14:37:35 +0530

    add contact us and fix navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 var myNav = document.querySelector("nav");
-window.onscroll = function () { 
-    "use strict";
-    if (document.scrollingElement.scrollTop >= document.getElementById("main").clientHeight ) {
+// window.onscroll = function () { 
+//     "use strict";
+//     if (document.scrollingElement.scrollTop >= document.getElementById("main").clientHeight ) {
+//         myNav.classList.add("nav-colored");
+//         myNav.classList.remove("nav-transparent");
+//     } 
+//     else {
+//         myNav.classList.add("nav-transparent");
+//         myNav.classList.remove("nav-colored");
+//     }
+// };
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function doSomething(scroll_pos) {
+  // do something with the scroll position
+
+  if (scroll_pos >= document.getElementById("main").clientHeight ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
     } 
@@ -9,4 +25,21 @@ window.onscroll = function () {
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
     }
-};
+}
+
+window.addEventListener('scroll', function(e) {
+
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+     
+    ticking = true;
+
+  }
+  
+});
